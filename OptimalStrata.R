20e1c9f15dd2f1b784b838087ed2468dde464858
Derek Corcoran <au687614@uni.au.dk>
2021-06-19 17:00:36 +0200

    Groups ready

--- OptimalStrata.R
+++ OptimalStrata.R
@@ -2,6 +2,7 @@ library(sf)
 library(raster)
 library(tidyverse)
 library(vegan)
+library(janitor)
 
 Husby <- readRDS("HusbyRaster.rds")
 
@@ -14,6 +15,28 @@ TestHusby <- cascadeKM(ForK[,-1],inf.gr = 2, sup.gr = 10, iter = 100)
 
 plot(TestHusby)
 
+Results <- TestHusby$results %>% t %>% 
+  as.data.frame()
+
+Results$Groups <- make_clean_names(rownames(Results))
+
+Selected <- Results %>% 
+  dplyr::filter(calinski == max(calinski)) %>% 
+  pull(Groups)
+
+Partition <- TestHusby$partition %>% 
+  as.data.frame() %>% 
+  clean_names() %>% 
+  pull(Selected)
+
+Groups <- Husby[[1]]
+
+values(Groups)[ForK$ID] <- Partition  
+values(Groups) <- ifelse(values(Groups) > length(unique(Partition)), NA, values(Groups))
+
+plot(Groups, colNA = "black")
+
+saveRDS(Groups, "GroupsHusby.rds")
 saveRDS(TestHusby, "StrataHusby.rds")
 
 Trojborg <- readRDS("TrojborgRaster.rds")
@@ -26,5 +49,28 @@ ForK <- as.data.frame(Trojborg) %>%
 TestTrojborg <- cascadeKM(ForK[,-1],inf.gr = 2, sup.gr = 10, iter = 100)
 
 plot(TestTrojborg)
+
+Results <- TestTrojborg$results %>% t %>% 
+  as.data.frame()
+
+Results$Groups <- make_clean_names(rownames(Results))
+
+Selected <- Results %>% 
+  dplyr::filter(calinski == max(calinski)) %>% 
+  pull(Groups)
+
+Partition <- TestTrojborg$partition %>% 
+  as.data.frame() %>% 
+  clean_names() %>% 
+  pull(Selected)
+
+Groups <- Trojborg[[1]]
+
+values(Groups)[ForK$ID] <- Partition  
+values(Groups) <- ifelse(values(Groups) > length(unique(Partition)), NA, values(Groups))
+
+plot(Groups, colNA = "black")
+
+saveRDS(Groups, "GroupsTrojborg.rds")
 
 saveRDS(TestTrojborg, "StrataTrojborg.rds")
